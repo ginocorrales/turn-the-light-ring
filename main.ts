377aac2d63bde6a5c96e70ec1962592b2e792f64
Gino Corrales <ginocorrales@users.noreply.github.com>
2023-05-05 09:19:04 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-let LED = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
+let LED = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGBW)
 LED.clear()
-LED.range(0, 9).showRainbow(0, 255)
-LED.setBrightness(100)
+LED.range(0, 9).showColor(neopixel.colors(NeoPixelColors.Indigo))
+LED.setBrightness(50)
 basic.pause(50)
 basic.forever(function () {
     LED.rotate(1)
